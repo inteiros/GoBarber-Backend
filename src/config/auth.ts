d6bfddd86fb8448bbb548249f2913249278687f2
JoaoEler <jgelermendes@gmail.com>
2020-05-05 22:25:53 -0300

    initial commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: 'e0e31b2d33f69619fbe36873965d83ed',
+        expiresIn: '1d',
+    },
+};
---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,40 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import Appointment from '../models/Appointment';
+import AppError from '../errors/AppError';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+    provider_id: string;
+    date: Date;
+}
+
+class CreateAppointmentService {
+    public async execute({ provider_id, date }: Request): Promise<Appointment> {
+        const appointmentsRepository = getCustomRepository(
+            AppointmentsRepository,
+        );
+
+        const appointmentDate = startOfHour(date);
+
+        const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+            appointmentDate,
+        );
+
+        if (findAppointmentInSameDate) {
+            throw new AppError('Já há marcações para este horário.');
+        }
+
+        const appointment = appointmentsRepository.create({
+            provider_id,
+            date: appointmentDate,
+        });
+
+        await appointmentsRepository.save(appointment);
+
+        return appointment;
+    }
+}
+
+export default CreateAppointmentService;
