70a25b82f310232ff7b689aff0a531cdd3aea501
JoaoEler <jgelermendes@gmail.com>
2020-05-09 20:52:29 -0300

    Forgotten email TDD

---
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,33 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+
+describe('SendForgotPasswordEmail', () => {
+    it('should be able to recover the password via e-mail', async () => {
+        const fakeUserRepository = new FakeUsersRepository();
+
+        const fakeMailProvider = new FakeMailProvider();
+
+        const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+        const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+            fakeUserRepository,
+            fakeMailProvider,
+        );
+
+        await fakeUserRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        await sendForgotPasswordEmail.execute({
+            email: 'johndoe@example.com',
+        });
+
+        expect(sendMail).toHaveBeenCalled();
+    });
+});
