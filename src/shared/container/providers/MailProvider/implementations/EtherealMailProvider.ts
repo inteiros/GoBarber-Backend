3209f982b8b3fbe44ad7c01059674ae6e2f3ed65
JoaoEler <jgelermendes@gmail.com>
2020-05-10 17:56:26 -0300

    Nodemailer, Ethereal, Mail Parsing

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,5 +1,34 @@
+import nodemailer, { Transporter } from 'nodemailer';
 import IMailProvider from '../models/IMailProvider';
 
 export default class EtherealMailProvider implements IMailProvider {
-    public async sendMail(to: string, body: string): Promise<void> {}
+    private client: Transporter;
+
+    constructor() {
+        nodemailer.createTestAccount().then(account => {
+            const transporter = nodemailer.createTransport({
+                host: account.smtp.host,
+                port: account.smtp.port,
+                secure: account.smtp.secure,
+                auth: {
+                    user: account.user,
+                    pass: account.pass,
+                },
+            });
+
+            this.client = transporter;
+        });
+    }
+
+    public async sendMail(to: string, body: string): Promise<void> {
+        const message = await this.client.sendMail({
+            from: 'Equipe GoBarber <equipe@gobarber.com.br>',
+            to,
+            subject: 'Recuperação de senha',
+            text: body,
+        });
+
+        console.log('Message sent: %s', message.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+    }
 }
