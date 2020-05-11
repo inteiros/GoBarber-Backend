905c23ab5acd65268be22953302e277ea36cde01
JoaoEler <jgelermendes@gmail.com>
2020-05-11 16:07:55 -0300

    listing providers

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,48 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import AppError from '@shared/errors/AppError';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUserRepository: FakeUsersRepository;
+let listProfiles: ListProvidersService;
+
+describe('ListProviders', () => {
+    beforeEach(() => {
+        fakeUserRepository = new FakeUsersRepository();
+
+        listProfiles = new ListProvidersService(fakeUserRepository);
+    });
+
+    it('should be able to list the providers', async () => {
+        const user1 = await fakeUserRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        const user2 = await fakeUserRepository.create({
+            name: 'John Trê',
+            email: 'johntre@example.com',
+            password: '123456',
+        });
+
+        const loggedUser = await fakeUserRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        const providers = await listProfiles.execute({
+            user_id: loggedUser.id,
+        });
+
+        expect(providers).toEqual([user1, user2]);
+    });
+
+    it('should not be able to show the profile from a non-existing user', async () => {
+        await expect(
+            listProfiles.execute({
+                user_id: 'non-existing-user_id',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+});
--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -3,6 +3,7 @@ import { uuid } from 'uuidv4';
 import IUsersRespository from '@modules/users/repositories/IUsersRepository';
 
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 
@@ -13,6 +14,18 @@ class FakeUsersRepository implements IUsersRespository {
         const findUser = this.users.find(user => user.id === id);
 
         return findUser;
+    }
+
+    public async findAllProviders({
+        except_user_id,
+    }: IFindAllProvidersDTO): Promise<User[]> {
+        let { users } = this;
+
+        if (except_user_id) {
+            users = this.users.filter(user => user.id !== except_user_id);
+        }
+
+        return users;
     }
 
     public async findByEmail(email: string): Promise<User | undefined> {
