5edc09615c2fe0f31a35e12e2a0ed2534bd71b41
JoaoEler <jgelermendes@gmail.com>
2020-05-12 23:01:14 -0300

    Validation

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersControllers';
 import ProviderMonthAvailabillityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController';
@@ -14,13 +15,24 @@ const providerDayAvailabilityController = new ProviderDayAvailabillityController
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+
 providersRouter.get(
     '/:provider_id/month-availability',
+    celebrate({
+        [Segments.PARAMS]: {
+            provider_id: Joi.string().uuid().required(),
+        },
+    }),
     providerMonthAvailabilityController.index,
 );
 
 providersRouter.get(
     '/:provider_id/day-availability',
+    celebrate({
+        [Segments.PARAMS]: {
+            provider_id: Joi.string().uuid().required(),
+        },
+    }),
     providerDayAvailabilityController.index,
 );
 
